1dfacedcc2aed8db21b187b7b0545711556d79f1
Vaibhav Hiware <hiwarevaibhav@gmail.com>
2020-12-23 15:57:36 +0530

    Initial Commit

---
+++ CSharpPrograms/CommanPrograms.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpPrograms
+{
+    class CommanPrograms
+    {
+        public void ReverseString(string str)
+        {
+            char[] charArray = str.ToCharArray();
+            for (int i = 0, j = str.Length-1; i < j; i++, j--)
+            {
+                charArray[i] = str[j];
+                charArray[j] = str[i];
+            }
+            string reversedString = new string(charArray);
+            Console.WriteLine("Reverse of string " + str + " is " + reversedString);
+        }
+
+        public void Palindrome(string str)
+        {
+            bool status = false;
+            for (int i = 0, j = str.Length-1; i < str.Length/2; i++, j--)
+            {
+                if(str[i] != str[j])
+                {
+                    status = false;
+                    break;
+                }
+                else
+                {
+                    status = true;
+                }
+            }
+
+            if(status)
+                Console.WriteLine(str + " is Palindrome");
+            else
+                Console.WriteLine(str + " is not Palindrome");
+        }
+
+        public void ReverseWordsOfString(string str)
+        {
+            string[] splitedWords = str.Split(' ');
+            Array.Reverse(splitedWords);
+            for (int i = 0; i <= splitedWords.Length - 1; i++)
+            {
+                Console.Write(splitedWords[i] + "" + ' ');
+            }
+        }
+    }
+}
---
+++ CSharpPrograms/MainClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSharpPrograms
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            CommanPrograms commanPrograms = new CommanPrograms();
+            commanPrograms.ReverseString("Hiware");
+            commanPrograms.Palindrome("19");
+            commanPrograms.ReverseWordsOfString("Hi i am vaibhav");
+        }
+    }
+}
