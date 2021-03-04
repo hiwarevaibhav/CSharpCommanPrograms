11d82640985f5421e83f8fe3e0b4121990001943
Vaibhav Hiware <hiwarevaibhav@gmail.com>
2021-03-04 08:37:32 +0530

    Fixed review comments

--- CSharpPrograms/MainClass.cs
+++ CSharpPrograms/MainClass.cs
@@ -7,16 +7,14 @@ namespace CSharpPrograms
         static void Main(string[] args)
         {
             CommanPrograms commanPrograms = new CommanPrograms();
-            //commanPrograms.ReverseString("Hiware");
-            //commanPrograms.Palindrome("19");
-            //commanPrograms.ReverseWordsOfString("Hi i am vaibhav");
-            //commanPrograms.CountCharactersInString("abbcccddddhhhhhhhhhhh");
-            //commanPrograms.Anagram("Silent", "Listen");
-            //commanPrograms.Factorial(6);
-            //commanPrograms.FibonacciSeries(7);
-            //commanPrograms.RemoveDuplicates("vaibhav");
-            
-            
+            commanPrograms.ReverseString("Hiware");
+            commanPrograms.Palindrome("19");
+            commanPrograms.ReverseWordsOfString("Hi i am vaibhav");
+            commanPrograms.CountCharactersInString("abbcccddddhhhhhhhhhhh");
+            commanPrograms.Anagram("Silent", "Listen");
+            commanPrograms.Factorial(6);
+            commanPrograms.FibonacciSeries(7);
+            commanPrograms.RemoveDuplicates("vaibhav");
         }
     }
 }
